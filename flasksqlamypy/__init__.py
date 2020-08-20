dd369b050970bc1fd39f6d001c0ec48a8206af8b
Bence Nagy <bence@underyx.me>
2020-08-20 15:58:06 +0200

    Remove py 3.8 only feature

--- flasksqlamypy/__init__.py
+++ flasksqlamypy/__init__.py
@@ -1,14 +1,11 @@
 from typing import Type
-from typing_extensions import Final
 
 from mypy.plugin import Plugin
 
 from .plugin import FlaskSQLAlchemyPlugin
 
-from importlib import metadata as importlib_metadata
 
-
-__version__: Final[str] = "0.0.1"
+__version__ = "0.0.1"
 
 
 def plugin(version: str) -> Type[Plugin]:
